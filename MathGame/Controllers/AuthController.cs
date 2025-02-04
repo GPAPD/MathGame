43f857d0e836b29a8713046afeb57867be388140
akash <akash@AKASH>
2025-02-04 10:09:01 +0530

    Add project files.

---
+++ MathGame/Controllers/AuthController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MathGame.Models;
+
+namespace MathGame.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            LogInRequestModel model = new LogInRequestModel(); 
+
+            return View("Login",model);
+        }
+    }
+}
---
+++ MathGame/Models/LogInRequestModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MathGame.Models
+{
+    public class LogInRequestModel
+    {
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
